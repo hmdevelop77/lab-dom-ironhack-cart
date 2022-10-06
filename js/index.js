e88e72c2cfd1e590b3b48386e0a3b86602cfbe0e
Oussama Hamdani <hmweb77@gmail.com>
2022-10-06 18:35:54 +0100

    done

--- js/index.js
+++ js/index.js
@@ -2,22 +2,33 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotalPrice = price * quantity;
+  product.querySelector('.subtotal span').innerText = subtotalPrice;
+  return subtotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  const rubberDuck = document.querySelector('.product1'); // why when we use getElementsByClassName doesn't work
+  const beachTowel = document.querySelector('.product2');
+  updateSubtotal(rubberDuck);
+  updateSubtotal(beachTowel);
   // ITERATION 3
   //... your code goes here
+  const rubberDuckPrice = updateSubtotal(rubberDuck);
+  const beachTowelPrice = updateSubtotal(beachTowel);
+  const totalFinalPrice = document.getElementById('total-value');
+  const totalPrice = rubberDuckPrice + beachTowelPrice;
+  totalFinalPrice.innerText = totalPrice;
 }
 
 // ITERATION 4
